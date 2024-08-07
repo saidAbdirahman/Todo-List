da9f4224d6fa0bd9047eb47c73a60d3f35e58774
saidAbdirahman <saidqaaje11@gmail.com>
2024-08-08 00:40:27 +0200

    AddItem
    Signed-off-by: saidAbdirahman <saidqaaje11@gmail.com>

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,20 @@
 import './bootstrap';
 import { createApp } from 'vue';
 import app from './components/app.vue';
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
 
-createApp(app).mount('#app')
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+/* import specific icons */
+import { faPlusSquare,faTrash } from '@fortawesome/free-solid-svg-icons'
+
+/* add icons to the library */
+library.add(faPlusSquare,faTrash)
+// createApp.component('font-awesome-icon', FontAwesomeIcon)
+
+
+createApp(app)
+  .component('font-awesome-icon', FontAwesomeIcon)
+  .mount('#app')
--- resources/js/components/addItemForm.vue
+++ resources/js/components/addItemForm.vue
@@ -1,13 +1,71 @@
 <template>
-    <div>
-        form
+    <div class="addItem">
+        <input type="text" v-model="item.name" />
+        <font-awesome-icon 
+        icon="plus-square" 
+       @click="addItem()"
+        :class="[item.name ? `active`:`inactive`,`plus`]"
+        />
     </div>
 </template>
 <script>
-
+import axios from 'axios';
+import { ref } from 'vue';
 export default {
     setup() {
-        
-    },
+      const item = ref({
+            name: ''
+        });
+       
+
+        return {item  }
+   
+},
+
+methods:{
+        addItem() {
+            if(this.item.name = ''){
+                return;
+            }
+            axios.post('/api/item/store', {
+                item:this.item.name
+            })
+            .then(response => {
+                if(response.status == 201){
+                    this.item.name = ''
+                }
+            })
+            .catch(error => {
+                console.log(error)
+            })
+        }
+    }
 }
 </script>
+
+<style scoped>
+.addItem{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+}
+
+input {
+background-color: #f7f7f7;
+border: 0px;
+margin-right: 10px;
+outline: none;
+padding: 5px;
+width: 100%;
+}
+.plus{
+    font-size: 20px;
+}
+.active{
+    color:#00CE25;
+}
+.inactive{
+    color:#999999
+}
+</style>
